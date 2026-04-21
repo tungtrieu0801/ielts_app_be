c6ddaa46e429fb22974b85572749ae8f29ef3db4
tungtrieu0801 <trieutungvp@gmail.com>
2026-04-21 09:40:18 +0700

    fix: bug fix connect database

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,14 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.DB_HOST}/${process.env.DB_NAME}`, {
-            auth: {
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-            },
-            authSource: "admin",
-        });
+        await mongoose.connect(process.env.DB_URI);
 
-        console.log("✅ MongoDB connected");
+        console.log('✅ MongoDB connected');
     } catch (err) {
-        console.error("❌ MongoDB connection error:", err);
+        console.error('❌ MongoDB connection error:', err);
         process.exit(1);
     }
 };
 
-export default connectDB;
+module.exports = connectDB;
