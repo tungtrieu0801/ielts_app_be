b782f884a350781592073bc8ac9b5387536ab008
tungtrieu0801 <trieutungvp@gmail.com>
2026-04-21 10:08:35 +0700

    feat: implement JWT authentication middleware to verify user tokens

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -11,7 +11,7 @@ export const verifyToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        // decoded: { id (googleId), email, name }
+        // decoded: { id (googleId), email, name,... }
         req.user = decoded;
         next();
     } catch (err) {
